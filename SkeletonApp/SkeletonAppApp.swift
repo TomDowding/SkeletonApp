e867222e1f7c5d69ff193451046eb2cdc0ef6aa7
Tom Dowding <tom.dowding@gmail.com>
2023-12-14 08:30:13 +0100

    Initial Commit

---
+++ SkeletonApp/SkeletonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonAppApp.swift
+//  SkeletonApp
+//
+//  Created by Tom Dowding on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
